a80d9aa6dc1f70be4393b3beaa3cee8edea7ed98
Sabine Eitel <Seit201@aol.com>
2022-11-29 10:42:12 +0100

    bee

---
+++ com/cc/java/Queen.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Queen {
+    public String doYourJob() {
+
+        return "I'm a Honeybee!";
+
+    }
+
+
+
+    public String fly() {
+
+        return "I can fly!";
+
+    }
+}
