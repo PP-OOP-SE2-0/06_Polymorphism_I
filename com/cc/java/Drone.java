453920123e18962d95d6df32309b22027bd41c50
Sabine Eitel <Seit201@aol.com>
2022-11-29 10:42:00 +0100

    bee

---
+++ com/cc/java/Drone.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class Drone {
+    public String doYourJob() {
+
+        return "I'm a Honeybee!";
+
+    }
+
+
+
+    public String fly() {
+
+        return "I can fly!";
+
+    }
+}
