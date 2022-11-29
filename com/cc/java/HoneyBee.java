47000a55b1032fc11d8001550ce833f13f6247a6
Sabine Eitel <Seit201@aol.com>
2022-11-29 10:42:06 +0100

    bee

---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,19 @@
+package com.cc.java;
+
+public class HoneyBee {
+    
+    public String doYourJob() {
+
+        return "I'm a Honeybee!";
+
+    }
+
+
+
+    public String fly() {
+
+        return "I can fly!";
+
+    }
+
+}
